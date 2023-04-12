e884a7cc78ccef7edb5fceb8951b2b1aedaa3daf
Tatiana Rem <tatiana13d@yandex.ru>
2023-04-12 20:46:55 +0600

    Time pattern was added

--- src/test/java/CardDeliveryTest.java
+++ src/test/java/CardDeliveryTest.java
@@ -10,20 +10,21 @@ import static com.codeborne.selenide.Selenide.*;
 
 
 public class CardDeliveryTest {
-    LocalDate currentDate = LocalDate.now();
-    LocalDate meetingDate = currentDate.plusDays(3);
-    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+    public String generateDate(long addDays, String pattern) {
+        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
+    }
 
 
     @Test
     void shouldCheckPositive() {
+        String planningDate = generateDate(3, "dd.MM.yyyy");
         open("http://localhost:9999/");
         $("[data-test-id=city] input").setValue("Краснодар");
-        $("[data-test-id=date] .input__control").doubleClick().sendKeys(formatter.format(meetingDate));
+        $("[data-test-id=date] .input__control").doubleClick().sendKeys(planningDate);
         $("[data-test-id=name] input").setValue("Ольга Александрова");
         $("[data-test-id=phone] input").setValue("+79001112233");
         $(" [data-test-id=agreement]").click();
         $(withText("Забронировать")).click();
-        $(withText("Встреча успешно забронирована на")).shouldBe(Condition.appear, Duration.ofSeconds(15));
+        $(".notification__content").shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15)).shouldBe(Condition.visible);
     }
 }
